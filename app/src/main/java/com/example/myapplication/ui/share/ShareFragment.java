6bd10646fa86bd34952881bd1ed56e519a31593d
collinsr729 <collinsr729@yahoo.com>
2020-03-28 14:27:57 -0400

    Started on Scheduling
    Mostly layout with setup for future dev

--- app/src/main/java/com/example/myapplication/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/myapplication/ui/share/ShareFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -23,13 +24,21 @@ public class ShareFragment extends Fragment {
         shareViewModel =
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_schedule);
+//        shareViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        makeFakeData(root);
         return root;
+    }
+
+    private void makeFakeData(View root) {
+        final TextView textView = root.findViewById(R.id.monday_schedule);
+        textView.setText("8:30am   SUNY Oswego to Downtown\n10:30am  Downtown to SUNY Oswego");
+        final TextView textView1 = root.findViewById(R.id.tuesday_schedule);
+        textView1.setText("7:30am  Downtown to SUNY Oswego\n10:30am SUNY Oswego to Downtown\n12pm  Downtown to SUNY Oswego\n3:50pm SUNY Oswego to Downtown");
     }
 }
